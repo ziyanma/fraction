e77839d15e92ac3552a3e0f3a2e119bf3a905a43
Ziyan Ma <ziyanma@usc.edu>
2016-02-17 19:33:43 -0800

    add fraction.h

---
+++ fraction.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+Class fraction{
+	public:
+		fraction();
+		fraction(int no,de);
+		~fraction();
+		void print();
+		void simplify();
+		fraction operator=(fraction other);
+		fraction operator+(fraction other);
+		fraction operator*(fraction other);
+		fraction operator-(fraction other);
+		fraction operator/(fraction other);
+
+	private:
+		int a,b;
+		//a as nominator, b as denominator
+}
